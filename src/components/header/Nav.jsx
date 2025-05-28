a72b1f233818ae9602800d1de18f4b19b4f18e99
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-28 11:43:40 +0200

    burgir menu for tablet & phone okayge

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,22 +1,44 @@
 import { NavLink } from "react-router-dom";
 import "./nav.css";
 import logo from "../../assets/shared/logo.svg";
+import iconHamburger from "../../assets/shared/icon-hamburger.svg";
+import iconClose from "../../assets/shared/icon-close.svg";
+import { useState } from "react";
 
 export default function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <div className="nav-container">
         <NavLink to={"/"}>
           <img src={logo} alt="logo" id="logo" />
-          {/* <span className="logo-tooltip">Made with ❤️ by Kamil Baldyga</span> */}
         </NavLink>
-        <ul className="nav-ul">
+
+        <button
+          className="burger-nav-toggle"
+          aria-controls="nav-ul"
+          aria-expanded={menuOpen}
+          onClick={toggleMenu}
+        >
+          <img
+            src={menuOpen ? iconClose : iconHamburger}
+            alt={menuOpen ? "Close menu" : "Open menu"}
+          />
+        </button>
+
+        <ul className={`nav-ul ${menuOpen ? "open" : ""}`} id="nav-ul">
           <li>
             <NavLink
               to={"/"}
               className={({ isActive }) =>
                 isActive ? "li-link active" : "li-link"
               }
+              onClick={() => setMenuOpen(false)}
             >
               00 HOME
             </NavLink>
@@ -27,6 +49,7 @@ export default function Nav() {
               className={({ isActive }) =>
                 isActive ? "li-link active" : "li-link"
               }
+              onClick={() => setMenuOpen(false)}
             >
               01 DESTINATION
             </NavLink>
@@ -37,6 +60,7 @@ export default function Nav() {
               className={({ isActive }) =>
                 isActive ? "li-link active" : "li-link"
               }
+              onClick={() => setMenuOpen(false)}
             >
               02 CREW
             </NavLink>
@@ -47,6 +71,7 @@ export default function Nav() {
               className={({ isActive }) =>
                 isActive ? "li-link active" : "li-link"
               }
+              onClick={() => setMenuOpen(false)}
             >
               03 TECHNOLOGY
             </NavLink>
